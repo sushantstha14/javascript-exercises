4998ef1c3863359f439c6b3602bfcb92673acc25
Sushant <161119877+Sushpeace12@users.noreply.github.com>
2024-10-16 00:49:39 +0545

    add a function logic to repeat the number
    the text wont be duplicated without it.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text,times) {
+    let result = "";
+    for(i=0; i<times; i++){
+      result += text;
+    }
+    return result;
+    
+    
 };
+
+repeatString("hello",10);
 
 // Do not edit below this line
 module.exports = repeatString;
