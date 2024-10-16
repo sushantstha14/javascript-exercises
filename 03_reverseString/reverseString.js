0946c9c3bb1158814d08e26224d896a77085af39
Sushant <161119877+Sushpeace12@users.noreply.github.com>
2024-10-16 15:29:30 +0545

    create a function to reverse a word.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    const myArray = text.split("");
+    let popResult = "";
+    let result = "";
+    
+    for(i = 0;i < text.length;i++){
+         popResult = myArray.pop();
+         result += popResult;
+        
+    }
+    return result;
+    
 };
+
+reverseString(" ");
 
 // Do not edit below this line
 module.exports = reverseString;
