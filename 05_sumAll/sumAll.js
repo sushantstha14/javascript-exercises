b30bc951db4118626da3f396072a6da1d7e9e1b2
Sushant <161119877+Sushpeace12@users.noreply.github.com>
2024-10-19 15:25:28 +0545

    create a function to implement test cases.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+   
+    if(!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if(start < 0 || end < 0) return "ERROR";
+    if(start%2 != 0 || end % 2 != 0) return "ERROR";
+    if(start > end){
+        let temp = start;
+        start = end;
+        end = temp;  
+    }
+    let sum = 0;
+
+    for(i=start;i<=end;i++){
+        sum = sum + i;
+    }
+    return sum;
 };
+
+sumAll(10,"90");
 
 // Do not edit below this line
 module.exports = sumAll;
