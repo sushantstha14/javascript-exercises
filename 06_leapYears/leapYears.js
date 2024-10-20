99b33970078ed1f77ae7230b1da3f2a4d600b0b1
Sushant <161119877+Sushpeace12@users.noreply.github.com>
2024-10-20 18:53:50 +0545

    create a function to find leap year or not
    leap year wont be determined without it.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if(year%4 == 0){
+        if(year%100 == 0){
+            if(year%400 == 0){
+                return true;
+            }
+        return false;
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
 };
+
+leapYears(1600);
 
 // Do not edit below this line
 module.exports = leapYears;
